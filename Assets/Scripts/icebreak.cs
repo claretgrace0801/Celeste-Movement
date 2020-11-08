d1c4a0231f1d30888b1eb4cbfbb65c65a1083c6f
shreyas <claretgrace0801@gmail.com>
2020-11-09 05:20:05 +0530

    Made Ice breakers

---
+++ Assets/Scripts/icebreak.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class icebreak : MonoBehaviour
+{
+    public float shakeTime = 1f;
+    public float spawnTime = 1f;
+
+    IEnumerator destroyIce()
+    {
+        yield return new WaitForSeconds(shakeTime);
+        gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        Debug.Log("Ice destroyed");
+        yield return new WaitForSeconds(spawnTime);
+        gameObject.GetComponent<Renderer>().enabled = true; 
+        gameObject.GetComponent<Collider2D>().enabled = true;  
+        Debug.Log("Ice should be back");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.name == "Player" && collision.gameObject.transform.position.y > gameObject.transform.position.y)
+        {
+            StartCoroutine(destroyIce());
+        }
+    }
+}
